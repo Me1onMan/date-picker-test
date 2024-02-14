d0038889f400fd1bc84266caded2a9422b7bcafe
Fyodor Shklyar <30theflame05@gmail.com>
2024-02-14 10:56:04 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  CalendarManager,
+  DatePicker,
+  withWeekends,
+  withTodos,
+} from "date-picker-library-shklyar";
+
+const calendar = new CalendarManager();
+calendar.addDecorator(withWeekends);
+calendar.addDecorator(withTodos);
+const Calendar = calendar.getCalendar();
+
+const maxDate = new Date(2024, 1, 26);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DatePicker CalendarView={Calendar} maxDate={maxDate} />
     </div>
   );
 }
